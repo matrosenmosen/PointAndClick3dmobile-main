814b3904006179d5c0347a77bebfe93ba1b5564f
matrosenmosen <moa.l.bergstrom@gmail.com>
2023-11-14 16:12:50 +0100

    ver. 2

--- Assets/Scripts/ObjectRotate.cs
+++ Assets/Scripts/ObjectRotate.cs
@@ -6,16 +6,24 @@ public class ObjectRotate : MonoBehaviour
 {
 
     public float sensistivity = 3f;
+    public float touchSensitivity = 3f;
 
     private void OnMouseDrag()
     {
         float xAxisRotation = Input.GetAxis("Mouse X") * sensistivity;
         float yAxisRotation = Input.GetAxis("Mouse Y") * sensistivity;
         
+        if (Input.touchCount > 0) 
+        {
+            xAxisRotation = Input.touches[0].deltaPosition.x / touchSensitivity;
+            yAxisRotation = Input.touches[0].deltaPosition.y / touchSensitivity;
+        }
+
         transform.Rotate(Vector3.down, xAxisRotation);
         transform.Rotate(Vector3.right, yAxisRotation);
 
-    }
+        Debug.Log("rotates");
 
+    }
 
 }
